798829474c6aa1ab91ba05dd65b3d8e44d399648
余康云 <yukangyun@jczh100.com>
2026-05-05 20:05:49 +0800

    新增上传向量数据库

--- initdb-ai/src/main/java/cn/fish/initDB/event/listen/AgentKnowledgeListener.java
+++ initdb-ai/src/main/java/cn/fish/initDB/event/listen/AgentKnowledgeListener.java
@@ -5,6 +5,7 @@ import cn.fish.initDB.event.AgentKnowledgeAddEvent;
 import cn.fish.knowledge.entity.AgentKnowledge;
 import cn.fish.knowledge.repository.AgentKnowledgeRepository;
 import cn.fish.knowledge.repository.VectorStoreRepository;
+import cn.fish.knowledge.service.DocumentSplitter;
 import cn.hutool.core.util.ObjUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +17,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
@@ -25,6 +27,7 @@ public class AgentKnowledgeListener {
     private final VectorStoreRepository vectorStoreRepository;
     private final ServaFile servaFile;
     private final AgentKnowledgeRepository agentKnowledgeRepository;
+    private final DocumentSplitter documentSplitter;
 
 
     @Async
@@ -37,14 +40,8 @@ public class AgentKnowledgeListener {
         knowledge.uploading();
         try {
             File file = servaFile.getFile(knowledge.getKnowledgeInfo().getFileId());
-            // 1. 将 java.io.File 包装为 Spring Resource
-            FileSystemResource fileResource = new FileSystemResource(file);
-
-            // 2. 使用 Tika 文档读取器（自动识别所有文件格式）
-            TikaDocumentReader documentReader = new TikaDocumentReader(fileResource);
-
-            // 3. 读取并返回 Document 集合
-            List<Document> splitDocuments = documentReader.read();
+            List<Document> splitDocuments = splitDocumentByType(file, knowledge.getKnowledgeInfo().getSplitterType());
+            addMetadata(LocalDate.now(), splitDocuments);
             vectorStoreRepository.add(splitDocuments);
             knowledge.complete();
 
@@ -56,4 +53,32 @@ public class AgentKnowledgeListener {
             agentKnowledgeRepository.updateKnowledge(knowledge);
         }
     }
+
+    private List<Document> splitDocumentByType(File file, String splitterType) {
+        // 1. 将 java.io.File 包装为 Spring Resource
+        FileSystemResource fileResource = new FileSystemResource(file);
+
+        // 2. 使用 Tika 文档读取器（自动识别所有文件格式）
+        TikaDocumentReader documentReader = new TikaDocumentReader(fileResource);
+        List<Document> documents = documentReader.read();
+        // 	TOKEN("token"), RECURSIVE("recursive"), SENTENCE("sentence"), PARAGRAPH("paragraph"), SEMANTIC("semantic");
+        switch (splitterType) {
+            case "token":
+                return documentSplitter.tokenBasedSplit(documents);
+            case "recursive":
+                return documentSplitter.tokenBasedSplit(documents);
+            case "semantic":
+                return documentSplitter.semanticSplit(documents);
+            default:
+                return documentSplitter.tokenBasedSplit(documents);
+        }
+    }
+
+    private static void addMetadata(LocalDate sessionId, List<Document> splitDocuments) {
+        for (Document document : splitDocuments) {
+            document.getMetadata().put("split_date", sessionId);
+        }
+    }
+
+
 }
